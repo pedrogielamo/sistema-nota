4a862bea0f9e3265ac259ec48e91637a00ceb546
pedrogielamo <pedro.gielamo@edu.senai.br>
2026-05-20 10:49:28 -0300

    feat:Adicionado relatorios e classification

--- main.cpp
+++ main.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 int main() {
     SetConsoleOutputCP(65001);
-                // ENTRADA(declarando variaveis)
     string nomes[20];
     int qtdAlunos;
     float notas[20][5];
     float media [20];
     int qtdDisciplinas;
-                // PROCESSAMENTO
+    
     cout<<"=== SISTEMAS DE NOTAS v4.0 ==="<<endl;
     do{
         cout<<"Quantidade de alunos(1-20): " ;
@@ -33,19 +32,36 @@ int main() {
         float soma = 0;
         for (int j = 0; j < qtdDisciplinas; j ++){
             do{
-                cout << "Disciplina " << j + 1 << "(1 a 5): ";
+                cout << "Disciplina " << j + 1 << "(0 a 10): ";
                 cin >> notas[i][j];
-            } while(notas[i][j] < 1 || notas[i][j] > 5);
+            } while(notas[i][j] < 0 || notas[i][j] > 10);
             soma += notas[i][j];
         }
         media[i] = soma / qtdDisciplinas;
      }  
 
-                // SAIDA
     cout<<"\n Alunos cadastrados: " << endl;
          for (int i = 0; i <qtdAlunos; i++) {
             cout<< "  " << i + 1 << ". " << nomes[i] << endl;
-         }          
+         }    
+         cout <<"\n=== RELATÓRIO === " << endl;
+         int aprovados = 0, recuperacao = 0, reprovado = 0;
+            for(int i = 0; i < qtdAlunos; i ++){
+            cout << nomes[i] << " - media: " << media[i] << endl; " - ";
+            if(media[i] >= 7){
+                cout<<"Aprovado" << endl;
+            aprovados++;
+             } else if (media[i]>=5){
+                cout<<"Recuperação" << endl;
+                recuperacao++;
+             } else{
+                cout<<"Reprovado"<< endl;
+                reprovado++;
+             }
+            }
+            cout<<"\nResumo: " << aprovados << " aprovados, " << recuperacao << " em recuperação, " << reprovado << " reprovados" << endl;
+            
+             
 
     return 0;
 }
