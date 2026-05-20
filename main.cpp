7037528ccfa693b61f2fd43852ebfc99ac95ee4c
pedrogielamo <pedro.gielamo@edu.senai.br>
2026-05-20 10:25:03 -0300

    feat: adiciona notas e calculo de media

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main() {
                 // ENTRADA(declarando variaveis)
     string nomes[20];
     int qtdAlunos;
+    float notas[20][5];
+    float media [20];
+    int qtdDisciplinas;
                 // PROCESSAMENTO
     cout<<"=== SISTEMAS DE NOTAS v4.0 ==="<<endl;
     do{
@@ -21,6 +24,23 @@ int main() {
             cout << "Nome do aluno " << i + 1 << ":";
             getline(cin, nomes[i]);
         }
+    do{
+         cout<<"\nQuantidade de discplinas(1-5): ";
+        cin >> qtdDisciplinas;
+     } while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
+     for (int i = 0; i < qtdAlunos; i ++){
+        cout <<"\n Notas de " << nomes[i] << ":" << endl;
+        float soma = 0;
+        for (int j = 0; j < qtdDisciplinas; j ++){
+            do{
+                cout << "Disciplina " << j + 1 << "(1 a 5): ";
+                cin >> notas[i][j];
+            } while(notas[i][j] < 1 || notas[i][j] > 5);
+            soma += notas[i][j];
+        }
+        media[i] = soma / qtdDisciplinas;
+     }  
+
                 // SAIDA
     cout<<"\n Alunos cadastrados: " << endl;
          for (int i = 0; i <qtdAlunos; i++) {
